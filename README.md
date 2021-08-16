5f40c87060265864ad217edafc643fc1b0588e43
Roman Troy <vzhyx@ya.ru>
2021-08-17 01:42:09 +0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# i3-config
+My ubuntu + i3 config
+
+# Dependencies:
+i3, maim, wine (or clean it from mode 'open')
+
+# Credits:
+
+inspired by [@bubujka](https://github.com/Bubujka/bu.bin).
+
+
+# TODO
+
+1) Нейминг: почему .profile.d
+2)
---
+++ shellSnippets/cd.sh
@@ -0,0 +1,18 @@
+alias ..='cd ../'
+
+alias web-mail='cd ~/www/_mail'
+
+alias tmp='cd ~/.db/tmp'
+alias bin='cd ~/.bu.bin/bin'
+alias www='cd /home/www'
+alias aux='cd ~/.db/aux'
+alias shared='cd /home/shared'
+alias _='cd ~/.db/_'
+
+function prog(){
+        cd ~/.db/programming/$1
+}
+
+function life(){
+        cd ~/.db/life/$1
+}
